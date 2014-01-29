ee1e3cc3b46a323833ccdb61476488596b4c9b9b
melodicodyssey84 <stevedbauer@gmail.com>
2014-01-29 13:17:13 -0800

    commit for submittal

--- store/app/controllers/categories_controller.rb
+++ store/app/controllers/categories_controller.rb
@@ -1,43 +1,43 @@
 class CategoriesController < ApplicationController
   
   def index
-	@categories = Category.all
+	 @categories = Category.all
   end
 
   def new
-	@category = Category.new()
-	@products = Product.all
+	 @category = Category.new()
+	 @products = Product.all
   end
 
   def create
-	new_cat = Category.create_new(category_params, params[:checked])
-	redirect_to category_path(new_cat.id)
+	 new_cat = Category.create_new(category_params, params[:checked])
+	 redirect_to category_path(new_cat.id)
   end
 
   def show
-	@category = Category.find(params[:id])
+	 @category = Category.find(params[:id])
   end
 
   def edit
-  	@category = Category.find(params[:id])
-	@products = Product.all
+   @category = Category.find(params[:id])
+	 @products = Product.all
   end
 
   def update
-	up_cat = Category.update(category_params, params[:checked])
-	redirect_to category_path(up_cat.id)	
+	 up_cat = Category.update(category_params, params[:checked])
+	 redirect_to category_path(up_cat.id)	
   end
 
   def destroy
-  	id = params[:id]
-  	Category.destroy(id)
-  	redirect_to index_path
+   id = params[:id]
+   Category.destroy(id)
+   redirect_to index_path
   end
 
   private
 
   def category_params
-  	params.require(:category).permit(:name)
+   params.require(:category).permit(:name)
   end
 
 end
